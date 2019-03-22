7287cb2c6d25f62dd497ec3ae483d9ede50d72dc
root <root@Maxs-iPad.home>
2019-03-22 11:45:33 +0000

    dded quick check for 0, hopefully will speed things up a bit

--- persistance.py
+++ persistance.py
@@ -1,5 +1,5 @@
 import math
-total = 20000
+total = 40000
 
 def persistance(num):
     if num < 10:
@@ -11,20 +11,26 @@ def persistance(num):
     
 max_per = 0    
 while True:
+    print(f'starting {total}')
     for i in range(total + 1):
         for j in range(total - i + 1):
             k =  total - i - j
             num = 2**i * 3**j * 7**k
-            per = persistance(num)
+            if '0' in str(num):
+                per = 1
+            else:
+                per = persistance(num)
             if per > max_per:
                 max_per = per
                 print(f"2^{i}  * 3^{j} * 7^{k}, {per+1}")
             
             if i != 0: 
                 num = 5**i * 3**j * 7**k
-                per = persistance(num)
+                if '0' in str(num):                                                                                                                          
+                    per = 1                                                                                                                                
+                else:                                                                                                                                      
+                    per = persistance(num)
                 if per > max_per:
                     max_per = per
                     print(f"3^{j}  * 5^{i} * 7^{k}, {per+1}")
     total += 1
-    print(total)
